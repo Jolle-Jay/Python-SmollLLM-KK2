5a02bd00ced6a1f0b8772fe6184cbca56be533f6
Jolle-Jay <j.lopezzz@hotmail.com>
2026-05-26 14:34:33 +0200

    har fixat till så at pandas returnerar korrekt dataset med rows, columns och dtypes, lärde mig att i row så vill jag returnera 0 i indexeringen, columner så är det to_list inte list och samma med dtypes

--- app/data.py
+++ app/data.py
@@ -3,6 +3,11 @@ import io
 
 dataset = None
 
-def load_dataset(contents: bytes) -> None:
+def load_dataset(contents: bytes) -> dict:
   global dataset #gör så att dataset pandas inte bara skapas i funktionen
   dataset = pd.read_csv(io.BytesIO(contents)) # tar råa bytes och gör dem tillett objekt som beter sig som en fil
+  return {
+    "rows": dataset.shape[0],
+    "columns": dataset.columns.to_list(),
+    "dtypes": dataset.dtypes.apply(str).to_dict()
+  }
--- reflektion.md
+++ reflektion.md
@@ -1,3 +1,5 @@
 gjorde health för att kolla att det var up and running
 post data stats är för csv som pandas ska läsa in och analysera
 typannotering - contents: bytes
+
+många saker är självklara jag får det förklarat och hade rätt, bara jag kan inte uttrycka hela bilden med egna ord
