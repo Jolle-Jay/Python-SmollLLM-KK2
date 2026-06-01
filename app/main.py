d00e71873367ea9a70add5afeccfca8295e8f612
Jolle-Jay <j.lopezzz@hotmail.com>
2026-06-01 10:48:32 +0200

    klar med logger i main.py

--- app/main.py
+++ app/main.py
@@ -4,12 +4,15 @@ from . import data
 from .schemas import AskRequest
 from .chain.pipeline import oraklet
 from .chain.steps import PromptBuilderInput
+import logging
 
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 @app.get("/health")
 async def health():
+  logger.info(f"Health check requested")
   return{"status": "ok"}
 
 @app.post("/data/upload")
@@ -21,23 +24,28 @@ async def upload_data(file: UploadFile):
   if len(contents) > 524288000:
     raise HTTPException(status_code=400, detail="File is to big maximum is 500mb")
   metadata = load_dataset(contents)
+  logger.info(f"CSV uploaded: {file.filename}, size: {len(contents)} bytes")
   return metadata
 
 @app.get("/data/stats")
 async def get_stats():
   if data.dataset is None:
     raise HTTPException(status_code=404, detail="Gotta have a dataset uploaded")
+  logger.info(f"Got the {data} from CSV filme")
   return data.dataset.describe().to_dict()
 
 @app.post("/ai/ask")
 async def ask(body: AskRequest):
   if data.dataset is None:
+    logger.error(f"AI ask failed: No dataset uploaded")
     raise HTTPException(status_code=400, detail="Gotta have CSV uploaded")
   
   result = oraklet.invoke(PromptBuilderInput(
     question=body.question,
     stats=data.dataset.describe().to_dict()
   ))
+  logger.info(f"AI ask: question='{body.question}' ")
+
   return result
 
 
