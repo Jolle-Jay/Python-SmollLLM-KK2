e62f44a5c5bede631712e325e430e7cd7decdb44
Jolle-Jay <j.lopezzz@hotmail.com>
2026-06-01 14:01:11 +0200

    klar med andra testen, det var jobbigt, lärde mig hur jag får det att vara som ett csv i ett test och hur jag skulle asserta för att få det se ut som pandas med rows, colums och dtypes

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -8,3 +8,17 @@ def test_health():
   
   assert resp.status_code == 200
   assert resp.json()["status"] == "ok"
+
+def test_upload():
+  resp = client.post("/data/upload", files={"file": (
+    "Test.csv",
+    b"name,age\nAlice,30",
+    "text/csv"
+    )})
+  
+  assert resp.status_code == 200
+  assert resp.json()["rows"] == 1
+  assert resp.json()["columns"] == ["name", "age"]
+  assert resp.json()["dtypes"] == {"name": "str", "age": "int64"}
+
+
